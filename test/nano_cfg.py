16140c6b3cf66f9f89bfd35002444cbb40439dba
Andrea <andrea.rizzi@cern.ch>
2017-08-28 14:14:25 +0200

    add example that dereferences the Ptr

--- test/nano_cfg.py
+++ test/nano_cfg.py
@@ -31,7 +31,7 @@ process.muonTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = cms.InputTag("linkedObjects","muons"),
     cut = cms.string("pt > 5 && track.isNonnull && (isGlobalMuon || isTrackerMuon) && isPFMuon"),
     name= cms.string("Muon"),
-    floats = cms.PSet(P4Floats, dxy = cms.string("dB")),
+    floats = cms.PSet(P4Floats, dxy = cms.string("dB"), jetPt = cms.string("?hasUserCand('jet')?userCand('jet').pt():-1")),
     ints = cms.PSet(CandInts, nStations = cms.string("numberOfMatchedStations"), jet = cms.string("?hasUserCand('jet')?userCand('jet').key():-1")),
     bools = cms.PSet(mediumId = cms.string("isPFMuon && innerTrack.validFraction >= 0.49 && ( isGlobalMuon && globalTrack.normalizedChi2 < 3 && combinedQuality.chi2LocalPosition < 12 && combinedQuality.trkKink < 20 && segmentCompatibility >= 0.303 || segmentCompatibility >= 0.451 )")),
 )
