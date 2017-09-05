c3c35de33eec9cc2324cea0ac9b4451c06b6e2c9
Giovanni <gpetruc@gmail.com>
2017-09-05 10:29:25 +0200

    Basic MC matching of electrons, photons, muons, taus to genParticles

--- python/electrons_cff.py
+++ python/electrons_cff.py
@@ -71,5 +71,27 @@ slimmedElectronsWithUserData = cms.EDProducer("PATElectronUserDataEmbedder",
         ),
                                               )
 
+electronsMCMatchForTable = cms.EDProducer("MCMatcher",  # cut on deltaR, deltaPt/Pt; pick best by deltaR
+    src         = electronTable.src,                 # final reco collection
+    matched     = cms.InputTag("finalGenParticles"), # final mc-truth particle collection
+    mcPdgId     = cms.vint32(11,22),                 # one or more PDG ID (11 = el, 22 = pho); absolute values (see below)
+    checkCharge = cms.bool(False),              # True = require RECO and MC objects to have the same charge
+    mcStatus    = cms.vint32(1),                # PYTHIA status code (1 = stable, 2 = shower, 3 = hard scattering)
+    maxDeltaR   = cms.double(0.3),              # Minimum deltaR for the match
+    maxDPtRel   = cms.double(0.5),              # Minimum deltaPt/Pt for the match
+    resolveAmbiguities    = cms.bool(True),     # Forbid two RECO objects to match to the same GEN object
+    resolveByMatchQuality = cms.bool(True),    # False = just match input in order; True = pick lowest deltaR pair first
+)
+
+electronMCTable = cms.EDProducer("CandMCMatchTableProducer",
+    src     = electronTable.src,
+    mcMap   = cms.InputTag("electronsMCMatchForTable"),
+    objName = electronTable.name,
+    objType = electronTable.name, #cms.string("Electron"),
+    branchName = cms.string("mcMatch"),
+    docString = cms.string("MC matching to status==1 electrons or photons"),
+)
+
 electronSequence = cms.Sequence(egmGsfElectronIDSequence + slimmedElectronsWithUserData + finalElectrons)
 electronTables = cms.Sequence ( electronTable)
+electronMC = cms.Sequence(electronsMCMatchForTable + electronMCTable)
