62c771633399a6b835dd63ba8b87ac7a3c9dc5f1
Andrea <andrea.rizzi@cern.ch>
2017-09-08 12:33:06 +0200

    example macros to skim... to be enriched

---
+++ examples/skim.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "TTree.h"
+#include "TFile.h"
+#include "TH1F.h"
+
+void skim(std::string infile,std::string cut="Jet_pt>200")
+{
+  TFile *file=new TFile((infile+".root").c_str());
+  TTree *events=(TTree*)file->Get("Events");
+  TTree *lumi=(TTree*)file->Get("LuminosityBlocks");
+  TTree *runs=(TTree*)file->Get("Runs");
+  gBenchmark->Start("skim");
+  
+  std::cout<<"Copying new tree with "<<cut<<"... ";
+  TFile *outfile=new TFile((infile+"_Skim.root").c_str(), "recreate","",9);
+  outfile->cd();
+  TTree *events_new=(TTree*)events->CopyTree(cut.c_str());
+  TTree *lumi_new=(TTree*)lumi->Clone();
+  TTree *runs_new=(TTree*)runs->Clone();
+  std::cout<<"done cloning"<<std::endl;
+  events_new->Write();
+  lumi_new->Write();
+  runs_new->Write();
+  std::cout << "Input events: " << events->GetEntries() << " output Events: " << events_new->GetEntries() << "  skim efficiency " << 1.*events_new->GetEntries()/events->GetEntries() << std::endl;
+  outfile->Close();
+  gBenchmark->Print("skim");
+  
+  return;
+} 
